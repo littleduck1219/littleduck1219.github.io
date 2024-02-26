17595e649ec4fdd33c9da945701aebf6e83eee35
gyeong deok <littleduck1219@gmail.com>
2024-02-27 02:29:04 +0900

    [Feat]Category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,14 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
 import Introduction from 'components/Main/Introduction'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
@@ -15,6 +22,7 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   )
