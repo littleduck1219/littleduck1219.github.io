cd9a3385cfaa50a9daa12e94f8b9791534994773
gyeong deok <littleduck1219@gmail.com>
2024-03-05 01:50:53 +0900

    [Fix]mobile effect fix

--- src/components/Main/style.MyLink.ts
+++ src/components/Main/style.MyLink.ts
@@ -4,7 +4,7 @@ export const MyLinkWrapper = styled.div<{ offsetY: number; isMobile: boolean }>`
   position: fixed;
   right: 20px;
   top: 37%;
-  display: ${props => (props.isMobile ? 'none' : 'flex')};
+  display: none;
   flex-direction: column;
   gap: 10px;
   padding: 10px;
@@ -13,4 +13,8 @@ export const MyLinkWrapper = styled.div<{ offsetY: number; isMobile: boolean }>`
   background-color: #c1c2b8;
   transition: transform 0.3s ease-out;
   transform: translateY(${props => props.offsetY * 0.1}px);
+
+  @media (min-width: 1081px) {
+    display: flex;
+  }
 `;
